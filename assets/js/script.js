0a95965eb7a9b810a1af09110b5fee4cb99879aa
Bruno Carlos Soares <122658439+BrunoCSoares@users.noreply.github.com>
2024-11-21 15:32:05 -0300

    home e animação pronta

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+/* ========= Home ========= */
+/* ANIMAÇÃO */
+const button = document.querySelector("#home button");
+const rotacao = document.getElementById("rotacao");
+const gota = document.getElementById("gota_animada");
+
+button.addEventListener("mouseenter", () => {
+    rotacao.style.transform = "rotate(-20deg)";
+    gota.classList.add("hidden");
+    button.classList.add("hover");  
+});
+
+button.addEventListener("mouseleave", () => {
+    rotacao.style.transform = "rotate(0deg)";
+    gota.classList.remove("hidden");  
+    button.classList.remove("hover"); 
+});
+
+/* Mesma animação para mobile (não tem mouse) */
+rotacao.addEventListener("click", () => {
+    rotacao.style.transform = "rotate(-20deg)";
+    gota.classList.add("hidden");
+    button.classList.add("hover");  
+    setTimeout(() => {
+        rotacao.style.transform = "rotate(0deg)";
+        gota.classList.remove("hidden");
+        button.classList.remove("hover");  
+    }, 3000); // 3000ms = 3 segundos
+});
