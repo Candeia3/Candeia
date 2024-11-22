331f6e1ca1204227b1953bac84c16f0b9921dc5f
Bruno Carlos Soares <122658439+BrunoCSoares@users.noreply.github.com>
2024-11-22 07:10:50 -0300

    script interno pra resolver gtihubpages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,5 @@
 /* ========= LOGIN_CADASTO ========= */
 if (window.location.pathname.includes('login_cadastro.html')) {
-    var erro = document.getElementById('erro');
-    var manterLogado = document.getElementById('manter_logado');
     /* escolher formulário */
     const tipoUsuario = document.getElementById("consu_parse");
     const tipoSessao = document.getElementById("log_cad");
@@ -46,141 +44,10 @@ if (window.location.pathname.includes('login_cadastro.html')) {
     // Adicionar event listeners para os checkboxes
     tipoUsuario.addEventListener("change", atualizarVisibilidade);
     tipoSessao.addEventListener("change", atualizarVisibilidade);
-    
-    /* Validação de usuário */
-    if (sessionStorage.getItem('msgErro') == '1') {
-        erro.innerText = 'Realize o login para acessar o perfil';
-    }
-    function validarLogin() {
-        if (tipoUsuario.checked) {
-            var email = document.getElementById('email_login_parse');
-            var senha = document.getElementById('senha_login_parse');
-            var erroEmail = document.getElementById('erroemail_login_parse');
-            var erroSenha = document.getElementById('errosenha_login_parse');
-        }else {
-            var email = document.getElementById('email_login_consu');
-            var senha = document.getElementById('senha_login_consu');
-            var erroEmail = document.getElementById('erroemail_login_consu');
-            var erroSenha = document.getElementById('errosenha_login_consu');
-        }
-        sessionStorage.setItem('msgErro','0');
-        erro.innerText = '';
-
-        if (email.value == '') {
-            erro.innerText = '';
-            erroEmail.innerText = 'Campo "e-mail" obrigatório';
-            email.focus();
-        }else {
-            erroEmail.innerText = ""
-            if(senha.value == ''){
-                erroSenha.innerText = 'Campo "Senha" obrigatório';
-                senha.focus();
-            }else {
-                erroSenha.innerText = ""
-                if(email.value == localStorage.getItem('email') && senha.value == localStorage.getItem('senha')) {
-                    if(manterLogado.checked) {
-                        localStorage.setItem('logado','true');
-                    }else {
-                        sessionStorage.setItem('logado','true');
-                    }
-                    window.location.href = 'coleta.html';
-                }else {
-                    erro.innerText = "Email e/ou senha incorretos!"
-                    senha.focus();
-                }
-            }
-        }
-    }
-    function validarCadastro() {
-        if (tipoUsuario.checked) {
-            var nome = document.getElementById('nome_parse');
-            var email = document.getElementById('email_cadastro_parse');
-            var cnpj_cpf = document.getElementById('cnpj');
-            var senha = document.getElementById('senha_cadastro_parse');
-            var confirmarSenha = document.getElementById('confirmarSenha_parse');
-            var erroNome = document.getElementById('erronome_parse');
-            var erroEmail = document.getElementById('erroemail_cadastro_parse');
-            var erroCnpj_Cpf = document.getElementById('errocnpj');
-            var erroSenha = document.getElementById('errosenha_cadastro_parse');
-            var erroConfirmarSenha = document.getElementById('erroconfirmarSenha_parse');
-        }else {
-            var nome = document.getElementById('nome_consu');
-            var email = document.getElementById('email_cadastro_consu');
-            var cnpj_cpf = document.getElementById('cpf');
-            var senha = document.getElementById('senha_cadastro_consu');
-            var confirmarSenha = document.getElementById('confirmarSenha_consu');
-            var erroNome = document.getElementById('erronome_consu');
-            var erroEmail = document.getElementById('erroemail_cadastro_consu');
-            var erroCnpj_Cpf = document.getElementById('errocpf');
-            var erroSenha = document.getElementById('errosenha_cadastro_consu');
-            var erroConfirmarSenha = document.getElementById('erroconfirmarSenha_consu');
-        }
-        
-        sessionStorage.setItem('msgErro','0');
-        erro.innerText = '';
-        
-        if (nome.value == ''){
-            erro.innerText = '';
-            erroNome.innerText = 'Campo "nome" obrigatório';
-            nome.focus();
-        }else {
-            erroNome.innerText = '';
-            if (email.value == ''){
-                erroEmail.innerText = 'Campo "e-mail" obrigatório';
-                email.focus();
-            }else {
-                erroEmail.innerText = "";
-                if (cnpj_cpf.value == '') {
-                    erroCnpj_Cpf.innerText = 'Campo "CPF/CNPJ" obrigatório';
-                    cnpj_cpf.focus();
-                } else {
-                    erroCnpj_Cpf.innerText = "";
-                    if(senha.value == ''){
-                        erroSenha.innerText = 'Campo "Senha" obrigatório';
-                        senha.focus();
-                    }else {
-                        erroSenha.innerText = "";
-                        if(confirmarSenha.value == ''){
-                            erroConfirmarSenha.innerText = 'Confirme sua senha ';
-                            confirmarSenha.focus();
-                        }else {
-                            erroConfirmarSenha.innerText = "";
-                            if (senha.value != confirmarSenha.value){
-                                erroConfirmarSenha.innerText = 'Senhas não conferem';
-                                confirmarSenha.focus();
-                            }else {
-                                erroConfirmarSenha.innerText = '';
-                                if(manterLogado.checked){
-                                    localStorage.setItem('logado','true');
-                                }else {
-                                    sessionStorage.setItem('logado','true');
-                                }
-                                localStorage.setItem('nome', nome.value);
-                                localStorage.setItem('email', email.value);
-                                localStorage.setItem('cnpj_cpf', cnpj_cpf.value);
-                                localStorage.setItem('senha', senha.value);
-                                window.location.href = 'coleta.html';
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
 }
 
 /* ========= Perfil ========= */
 if (window.location.pathname.includes('perfil.html')) {
-    if (localStorage.getItem('logado') != 'true' && sessionStorage.getItem('logado') != 'true') {
-        window.location.href = 'login_cadastro.html';
-        sessionStorage.setItem('msgErro', '1');
-    }
-    else {
-        sessionStorage.setItem('msgErro', '0');
-    }
-    function trocarPontos() {
-        window.location.href = 'descontos.html';
-    }
     var nomeUsuario = document.getElementById('nomeUsuario');
     nomeUsuario.innerText = localStorage.getItem('nome') || 'Nome do Usuário';
 
@@ -191,16 +58,6 @@ if (window.location.pathname.includes('perfil.html')) {
         editar.checked = false;
         localStorage.setItem('nome', alterarNome.value);
         nomeUsuario.innerText = localStorage.getItem('nome');
-    }
-
-    function sairConta() {
-        localStorage.removeItem('nome');
-        localStorage.removeItem('email');
-        localStorage.removeItem('cnpj_cpf');
-        localStorage.removeItem('senha');
-        localStorage.removeItem('logado');
-        sessionStorage.removeItem('logado');
-        window.location.href = 'login_cadastro.html';
     }
 }
 
